963dca35c3b1a443fceb5df662b63d545a8a3c6f
Sameem-baba <ssssameembaba@gmail.com>
2021-12-17 12:15:38 +0530

    links update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
     title: 'Netflix clone',
-    description: "Using Next,js, React, Node.js, Redux & Firebase you'll learn how to build a Full Stack Application - from start to finish. ",
+    description: "Using Next.js, React, Node.js, Redux & Firebase you'll learn how to build a Full Stack Application - from start to finish. ",
       image: '/images/1.png',
       tags: ['Next.js', 'Redux', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/Sameem-baba/netflix-next-js',
+    visit: 'https://netflix-next-js.vercel.app/',
     id: 0,
   },
   {
@@ -13,8 +13,8 @@ export const projects = [
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: '/images/2.png',
     tags: ['React', 'JavaScript', "Next.js"],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/Sameem-baba/Amazon-clone',
+    visit: 'https://amazon-clone-five-phi.vercel.app/',
     id: 1,
   },
   {
@@ -22,7 +22,7 @@ export const projects = [
     description: "In this project, I made a web 3.0 app using React and Thirdweb. I learnt how to mint an NFT and pulled data from openseas API.",
       image: '/images/3.png',
       tags: ['React', 'thirdWeb'],
-    source: 'https://google.com',
+    source: 'https://github.com/Sameem-baba/Cryptopunk',
     visit: 'https://google.com',
     id: 2,
   },
@@ -31,8 +31,8 @@ export const projects = [
     description: "In this project, I made a discord bot using the discord API they provide. The bot can give you details about a certain universities which the user provided in the Chat",
     image: '/images/4.png',
     tags: ['React', 'Node.js', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/Sameem-baba/21consequences-bot',
+    visit: 'https://discord.gg/FSb5KhWJ',
     id: 3,
   },
 ];
